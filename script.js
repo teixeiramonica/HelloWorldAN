160c66f21b86d5f63b87efd06fb66a248cc13524
teixeiramonica <tmonica@escola.pr.gov.br>
2025-06-30 18:42:02 -0300

    listas

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ elemento.textContent = nomeUsuario;
 }
 
 inserirNome ();
+
+let linguagens = ["Javascript", "Python", "C"];
+console.log(linguagens[0]);
+console.log(linguagens[1]);
+console.log(linguagens[2]);

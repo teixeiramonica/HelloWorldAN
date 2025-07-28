1667982c3ad8ca9265b4e93d3104dd595ffa4855
teixeiramonica <tmonica@escola.pr.gov.br>
2025-07-28 18:53:28 -0300

    ;

--- script.js
+++ script.js
@@ -8,4 +8,20 @@ inserirNome ();
 
 let linguagens = ["Javascript", "Python", "C"];
 const item = document.querySelector("#lista");
-item.textContent = linguagens[0];
+item.textContent = linguagens[2];
+
+let aluno1 = {
+  nome: "Gabriel",
+  idade: 13,
+  anoLetivo: "2ºsérie",
+  materiasFavoritas: ["Geografia","Programação","Arte"]
+}
+
+console.log(aluno1.nome); // “Gabriel”
+console.log(aluno1.idade); // 13
+console.log(aluno1.materiasFavoritas);
+// ["Geografia","Programação","Arte"]
+console.log(aluno1.materiasFavoritas[1]);
+
+const objeto = document.querySelector("#aluno");
+objeto.textContent = aluno1.nome;
